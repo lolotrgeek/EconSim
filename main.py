ca3fa4fdb5ed48b03a152b10e41375dd1e492195
lolotrgeek <jon@simplyenable.com>
2023-07-17 11:24:43 -0500

    chore: run from main

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-pass
+import subprocess
+from time import sleep
+from signal import SIGTERM
+import sys
+import os
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+files = [
+    parent_dir+'\\EconSim\\run_exchange.py',
+    parent_dir+'\\EconSim\\run_agents.py',
+    parent_dir+'\\EconSim\\run_government.py',
+    parent_dir+'\\EconSim\\run_clock.py',
+    parent_dir+'\\EconSim\\run_api.py',
+]
+
+
+if __name__ == '__main__':
+    try:
+        processes = []
+        for file in files:
+            process = subprocess.Popen(['python', file])
+            processes.append(process)
+
+        while True:
+            sleep(.1)
+    except KeyboardInterrupt:
+        print("attempting to close agents..." )
+        for process in processes:
+            process.send_signal(SIGTERM)
+        exit()
+
