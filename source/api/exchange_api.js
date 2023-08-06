45644156d1469d1b3167b812184461fedc99c2ff
lolotrgeek <jon@simplyenable.com>
2023-08-06 09:28:46 -0500

    refactor: modular and decoupled

--- source/api/exchange_api.js
+++ source/api/exchange_api.js
@@ -1,71 +1,10 @@
-const zmq = require('zeromq')
-const express = require('express')
+import express from 'express'
+import Requester from './Requester.js'
 const app = express()
 
-class Requester {
-    constructor(channel = '5570') {
-        this.channel = channel
-        this.socket = new zmq.Request
-        this.socket.connect(`tcp://127.0.0.1:${this.channel}`)
-        this.queue = []
-        this.processing = false
-        this.latest_result = {}
-        this.debug = false
-    }
-
-    async request_direct(topic, msg) {
-        try {
-            msg.topic = topic
-            this.socket.send(JSON.stringify(msg))
-            const reply = await this.socket.receive()
-            return JSON.parse(reply)
-        } catch (e) {
-            console.log("[Requester Error]", e, "Request:", msg)
-            return { topic, error: e.message }
-        }
-    }
-
-    async request(topic, msg) {
-        this.queue.push({ topic, msg })
-        this.processQueue()
-    }
-
-    parser(reply) {
-        try {
-            return JSON.parse(reply)
-        } catch (Parse_error) {
-            try {
-                return reply.toString()
-            } catch (error) {
-                return { error: "cannot parse reply"}
-            }
-        }
-        
-    }
-
-    async processQueue() {
-        if (this.processing) return
-        this.processing = true
-
-        while (this.queue.length > 0) {
-            const { topic, msg } = this.queue.shift()
-            try {
-                msg.topic = topic
-                this.socket.send(JSON.stringify(msg))
-                if (this.debug) {console.log("Received response for topic:", topic)}
-                const reply = await this.socket.receive()
-                this.latest_result = this.parser(reply)
-            } catch (e) {
-                console.log("[Requester Error]", e, "Request:", msg)
-            }
-        }
-
-        this.processing = false
-    }
-}
 
 app.get('/', (req, res) => {
-    res.send('hello')
+    res.send('This is the exchange api.')
 })
 
 const sim_time_requests = new Requester('5570')
