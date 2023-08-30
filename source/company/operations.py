0b228da18b8aae3c340c756d75a0538ff0f932f7
lolotrgeek <jon@simplyenable.com>
2023-08-30 11:04:25 -0500

    fix: unassigned var

--- source/company/operations.py
+++ source/company/operations.py
@@ -310,7 +310,7 @@ class Operations():
         self.net_income_ratio = round(self.net_income / self.revenue, 10)
         self.weighted_average_shs_out = outstanding_shares / 1
         self.weighted_average_shs_out_dil = shares_issued / 1
-        if weighted_average_shs_out_dil == 0: weighted_average_shs_out_dil = 1 # NOTE: outstanding shares cannot be 0
+        if self.weighted_average_shs_out_dil == 0: self.weighted_average_shs_out_dil = 1 # NOTE: outstanding shares cannot be 0
         self.eps = round(self.net_income / self.weighted_average_shs_out, 2)
         self.eps_diluted = round(self.net_income / self.weighted_average_shs_out_dil, 2)             
 
