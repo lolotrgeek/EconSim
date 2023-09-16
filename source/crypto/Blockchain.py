e94c821027237f47843ef6a41109886737e12ba5
lolotrgeek <jon@simplyenable.com>
2023-09-16 08:32:40 -0500

    chore: txn dt from self, fee refuse/accept, process confirmations count

--- source/crypto/Blockchain.py
+++ source/crypto/Blockchain.py
@@ -5,17 +5,19 @@ import pandas as pd
 class Blockchain():
     def __init__(self, datetime=None):
         seed = MempoolTransaction('init_seed', 0, 0, 'init_seed', 'init_seed', datetime)
+        self.no_fee = False
         seed.confirmed = True
         self.chain = [seed]
         self.mempool = MemPool()
         self.datetime = datetime
         self.total_transactions = 0
+        self.accumulated_fees = 0
         # self.new_block(transactions=[], previous_hash=1)
 
-    async def new_block(self, transactions, previous_hash=None, dt=None) -> dict:
+    async def new_block(self, transactions, previous_hash=None) -> dict:
         block = {
             'index': len(self.chain) + 1, 
-            'timestamp': dt, 
+            'timestamp': self.datetime, 
             'transactions': transactions, 
             'previous_hash': previous_hash or hash(self.chain[-1]),
         }
@@ -23,28 +25,30 @@ class Blockchain():
         self.chain.append(block)
         return block
     
-    async def add_transaction(self, asset:str, fee:float, amount:float, sender:str, recipient:str, dt) -> MempoolTransaction:
-        if(fee <= 0.0): return MempoolTransaction("error", 0, 0, "error", "error", dt)
+    async def add_transaction(self, asset:str, fee:float, amount:float, sender:str, recipient:str) -> MempoolTransaction:
+        if(fee <= 0.0): return MempoolTransaction(asset, 0, 0, "error", "refusing transaction: no fee", dt=self.datetime)
         self.total_transactions += 1
-        mempool_transaction = MempoolTransaction(asset, fee, amount, sender, recipient, dt)
+        mempool_transaction = MempoolTransaction(asset, fee, amount, sender, recipient, dt=self.datetime)
         self.mempool.transactions.append(mempool_transaction)
         return mempool_transaction
 
-    async def process_transactions(self, dt=None) -> None:
+    async def process_transactions(self) -> None:
         unconfirmed_transactions = self.mempool.get_pending_transactions()
         unconfirmed_transactions.sort(key=lambda x: x.fee, reverse=True)
         num_unconfirmed = len(unconfirmed_transactions)
+        confirmed = 0
         for index, transaction in enumerate(unconfirmed_transactions):
             # create a probablity distribution for confirmation based on the length of the mempool
             confirmation_odds = .9 - (index / num_unconfirmed)
             if random.random() < confirmation_odds:
+                confirmed += 1
                 transaction.confirmed = True
-                transaction.timestamp = dt # when the transaction was confirmed
+                transaction.timestamp = self.datetime # when the transaction was confirmed
+                self.accumulated_fees += transaction.fee
                 num_unconfirmed -= 1
                 self.chain.append(transaction)
-        # self.new_block(self.mempool.get_confirmed_transactions(), hash(self.chain[-1]), dt=self.datetime)
-        
         self.mempool.transactions = self.mempool.get_pending_transactions() # clear the mempool of confirmed transactions
+        return {'confirmed': confirmed, 'unconfirmed': len(self.mempool.transactions) }
     
     async def get_transactions_df(self) -> pd.DataFrame:
         return pd.DataFrame.from_records([t.to_dict() for t in self.chain]).set_index('dt')
