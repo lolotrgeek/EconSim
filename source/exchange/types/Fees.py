0c474c750fe68b44b5f108505f250cb56ee65abc
lolotrgeek <jon@simplyenable.com>
2023-09-16 08:35:59 -0500

    feature: fee collection

--- source/exchange/types/Fees.py
+++ source/exchange/types/Fees.py
@@ -5,6 +5,7 @@ class Fees():
         self.maker_fee_rate = 0.001  # Maker fee rate as a decimal (e.g., 0.001 = 0.1%)
         self.taker_fee_rate = 0.002  # Taker fee rate as a decimal (e.g., 0.002 = 0.2%)
         self.total_fee_revenue = 0.0  # Total fee revenue collected by the exchange
+        self.fees_collected = {}
 
     def taker_fee(self, volume) -> float:
         if self.waive_fees:
@@ -16,3 +17,16 @@ class Fees():
             return 0.0
         return volume * self.maker_fee_rate
         
+    def add_fee(self, asset: str, fee: float) -> dict:
+        if asset in self.fees_collected: 
+            self.fees_collected[asset] += fee
+        else: 
+            self.fees_collected[asset] = fee
+        return {asset: fee}
+    
+    def remove_fee(self, asset: str, fee: float) -> dict:
+        if asset in self.fees_collected: 
+            self.fees_collected[asset] -= fee
+        else: 
+            self.fees_collected[asset] = -fee
+        return {asset: fee}
