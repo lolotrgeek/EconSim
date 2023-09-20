0b89e7d21524263407c5f4ad8f9666b293195c38
lolotrgeek <jon@simplyenable.com>
2023-09-20 07:22:15 -0500

    fix: decimal rates

--- source/exchange/types/Fees.py
+++ source/exchange/types/Fees.py
@@ -1,20 +1,22 @@
+from decimal import Decimal, getcontext
 
 class Fees():
     def __init__(self):
+        getcontext().prec = 18
         self.waive_fees = True
-        self.maker_fee_rate = 0.001  # Maker fee rate as a decimal (e.g., 0.001 = 0.1%)
-        self.taker_fee_rate = 0.002  # Taker fee rate as a decimal (e.g., 0.002 = 0.2%)
-        self.total_fee_revenue = 0.0  # Total fee revenue collected by the exchange
+        self.maker_fee_rate = Decimal('0.001')  # Maker fee rate as a decimal (e.g., 0.001 = 0.1%)
+        self.taker_fee_rate = Decimal('0.002')  # Taker fee rate as a decimal (e.g., 0.002 = 0.2%)
+        self.total_fee_revenue = 0  # Total fee revenue collected by the exchange
         self.fees_collected = {}
 
     def taker_fee(self, volume) -> float:
         if self.waive_fees:
-            return 0.0
+            return 0
         return volume * self.taker_fee_rate
     
     def maker_fee(self, volume) -> float:
         if self.waive_fees:
-            return 0.0
+            return 0
         return volume * self.maker_fee_rate
         
     def add_fee(self, asset: str, fee: float) -> dict:
