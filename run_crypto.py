aceed1bb007483a083e727a850c0e1fc6b3eed39
lolotrgeek <jon@simplyenable.com>
2023-09-28 05:56:12 -0500

    fix: issue coins

--- run_crypto.py
+++ run_crypto.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from source.crypto.CryptoCurrency import CryptoCurrency
+from source.exchange.CryptoExchangeRequests import CryptoExchangeRequests
 from source.Messaging import Responder, Requester, Subscriber
 import asyncio
 from rich import print
@@ -7,13 +8,15 @@ asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 from source.utils._utils import dumps, string_to_time
 from Channels import Channels
 
-names = ['A', 'frXoX', 'wAt', 'Ayc', 'EXCAb', 'Qw', 'vbcY', 'ZM', 'j', 'nNLga', 'Ln', 'ao', 'k', 'icyJ', 'r', 'qk', 'BeHN', 'if', 'yAnL', 'sw']
+names = ['BTC', 'ETH']
 
-def generate_cryptos(names, requester, time) -> dict:
-    cryptos = {}
+async def generate_cryptos(names, requester, time) -> dict:
+    cryptos = {"USD": CryptoCurrency("USD", time, requester=requester)}
+    await cryptos["USD"].issue_coins([{'asset': cryptos["USD"].symbol ,'market_qty':1000 ,'seed_price':100 ,'seed_bid':.99, 'seed_ask':1.01}], 1_000_000_000)
     for name in names:
         crypto = CryptoCurrency(name, time, requester=requester)
         cryptos[crypto.symbol] = crypto
+        await crypto.issue_coins([{'asset': crypto.symbol ,'market_qty':1000 ,'seed_price':100 ,'seed_bid':.99, 'seed_ask':1.01}], 1_000_000_000)
     return cryptos
 
 async def run_crypto() -> None:
@@ -35,9 +38,10 @@ async def run_crypto() -> None:
                 return string_to_time(clock) 
 
         time = get_time()
-        cryptos = generate_cryptos(names, requester, time)
+        cryptos = await generate_cryptos(names, CryptoExchangeRequests(requester), time)
 
         async def callback(msg):
+            print(msg)
             if 'asset' in msg:
                 if msg['asset'] in cryptos:
                     if msg['topic'] == 'get_transactions': return dumps(await cryptos[msg['asset']].blockchain.get_transactions())
@@ -50,11 +54,16 @@ async def run_crypto() -> None:
                 else: return f'unknown asset {msg["asset"]}'    
             else: return f'unknown topic {msg["topic"]}'
 
+        # for crypto in cryptos:
+        #     print("issuing coins", crypto)
+        #     result = await cryptos[crypto].issue_coins([{'asset': cryptos[crypto].symbol ,'market_qty':1000 ,'seed_price':100 ,'seed_bid':.99, 'seed_ask':1.01}], 1_000_000_000)
+        #     print(result)
+
         while True:
             time = get_time()
             for crypto in cryptos:
                 await cryptos[crypto].next(time)
-            msg = await responder.respond(callback)
+            msg = await responder.lazy_respond(callback)
             if msg == None:
                 continue
 
