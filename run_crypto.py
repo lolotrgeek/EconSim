776003c0120a61a59f05151cce029fd707803f17
lolotrgeek <jon@simplyenable.com>
2023-10-25 04:40:24 -0500

    fix: no floats

--- run_crypto.py
+++ run_crypto.py
@@ -45,7 +45,7 @@ async def run_crypto() -> None:
                 if msg['asset'] in cryptos:
                     if msg['topic'] == 'get_transactions': return dumps(await cryptos[msg['asset']].blockchain.get_transactions())
                     if msg['topic'] == 'get_transaction': return dumps(await cryptos[msg['asset']].blockchain.get_transaction(msg['id']))
-                    elif msg['topic'] == 'add_transaction': return dumps((await cryptos[msg['asset']].blockchain.add_transaction(msg['asset'], float(msg['fee']), float(msg['amount']), msg['sender'], msg['recipient'])).to_dict())
+                    elif msg['topic'] == 'add_transaction': return dumps((await cryptos[msg['asset']].blockchain.add_transaction(msg['asset'], msg['fee'], msg['amount'], msg['sender'], msg['recipient'])).to_dict())
                     elif msg['topic'] == 'get_mempool': return dumps(await cryptos[msg['asset']].blockchain.get_mempool())
                     elif msg['topic'] == 'get_pending_transactions': return dumps(await cryptos[msg['asset']].blockchain.mempool.get_pending_transactions(to_dicts=True))
                     elif msg['topic'] == 'get_confirmed_transactions': return dumps(await cryptos[msg['asset']].blockchain.mempool.get_confirmed_transactions(to_dicts=True))
