abe34430facd1801d40d7b7ae674315c9f1c97d2
lolotrgeek <jon@simplyenable.com>
2023-12-13 15:09:43 -0600

    feat: wallet for defi traders

--- source/crypto/Wallet.py
+++ source/crypto/Wallet.py
@@ -3,15 +3,13 @@ parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(parent_dir)
 from source.utils._utils import prec, generate_address, dumps
 from source.Messaging import Responder, Requester
-from source.exchange.CryptoExchangeDeFiRequests import CryptoExchangeDefiRequests
+from source.exchange.DefiExchangeRequests import DefiExchangeRequests
 from Channels import Channels
 
 class Wallet():
     def __init__(self, name, requester=None):
         channels = Channels()
-        self.exchange_listener = Responder(channels.wallet_channel)
-        self.requester = Requester(channels.defi_channel)
-        self.exchange_messenger = CryptoExchangeDefiRequests(self.requester)
+        self.requester = requester
         self.address = generate_address()
         self.name = name
         self.signature_requests = []
@@ -39,23 +37,20 @@ class Wallet():
             return {'invalid': txn, 'msg': (f"Wallet does not have enough {txn['asset']} to pay fee and amount")}
         return {'valid': txn, 'msg': 'valid transaction'}
     
+    async def signature_request(self, txn):
+        self.signature_requests.insert(0,txn)
+        return {'msg': 'request received'}
+
     async def sign_txn(self, txn, decision=False):
         if (await self.check_txn(txn)) == False:
             msg = f'invalid transaction {txn}'
             decision = False
-        await self.exchange_messenger.send_signature(decision=decision, txn=txn)
-        
-    async def callback(self, msg) -> str:
-        if msg['topic'] == 'request_signature':
-            #TODO: need to handle routing to the correct wallet
-            if msg['wallet'] != self.address: return dumps({'error': 'wrong address'}) 
-            self.signature_requests.append(msg['txn'])
-            return dumps({'msg': 'request received'}) 
-            # option 1 - forward request with a requester to the agent
-            # option 2 - have agent subscribe to wallet channel and respond to requests
-            # option 3 - have agent directly holding wallet object, and have agent call wallet.sign_txn(txn) <-- trying this first
-        elif msg['topic'] == 'get_balance': return dumps((await self.get_balance(msg['asset'])))
-        else: return dumps({"warning":  f'unknown topic {msg["topic"]}'})
-        
+        await self.requester.send_signature(decision=decision, txn=txn)
+    
+    async def set_fee(self, asset):
+        # https://ethereum.stackexchange.com/questions/114743/estimate-transaction-fee-on-bsc
+        # from cryptocurrency get the latest fee 
+        # set max willing to pay
+        # check that wallet has enough to pay fee
 
-        
+        return self.assets[asset]['fee']
