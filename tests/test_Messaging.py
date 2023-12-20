3ffa39e83b356f9720bf22b7b3d4951325005c8a
lolotrgeek <jon@simplyenable.com>
2023-12-20 04:49:08 -0600

    test: _utils decimal encoder

--- tests/test_Messaging.py
+++ tests/test_Messaging.py
@@ -1,19 +1,8 @@
 import unittest
 import json
-from decimal import Decimal
-from source.Messaging import DecimalEncoder
+from decimal import *
+from source.Messaging import *
 
-class DecimalEncoderTest(unittest.TestCase):
-    def test_decimal_encoder(self):
-        # Test that DecimalEncoder encodes Decimal objects correctly
-        d = Decimal('3.14159265358979323846')
-        expected_output = '3.141592653589793239' # NOTE when run alone test will fail because the decimal context precision gets set in _utils test, we put the "correct" value here when run after the _utils test
-        output = json.dumps(d, cls=DecimalEncoder)
-        self.assertEqual(output.replace('"', ''), expected_output)
-
-    def test_decimal_encoder_with_other_objects(self):
-        # Test that DecimalEncoder falls back to the default encoder for non-Decimal objects
-        obj = {'key': 'value'}
-        expected_output = '{"key": "value"}'
-        output = json.dumps(obj, cls=DecimalEncoder)
-        self.assertEqual(output, expected_output)
+#TODO: check all the messaging methods
+class TestMessaging():
+    pass
--- tests/test__utils.py
+++ tests/test__utils.py
@@ -80,5 +80,20 @@ class TestUtilsTest(unittest.TestCase):
     def test_get_minimum(self):
         self.assertEqual(get_minimum(18), Decimal('0.000000000000000001'))
 
+class DecimalEncoderTest(unittest.TestCase):
+    def test_decimal_encoder(self):
+        # Test that DecimalEncoder encodes Decimal objects correctly
+        d = Decimal('3.14159265358979323846')
+        expected_output = '3.141592653589793239' # NOTE when run alone test will fail because the decimal context precision gets set in _utils test, we put the "correct" value here when run after the _utils test
+        output = json.dumps(d, cls=DecimalEncoder)
+        self.assertEqual(output.replace('"', ''), expected_output)
+
+    def test_decimal_encoder_with_other_objects(self):
+        # Test that DecimalEncoder falls back to the default encoder for non-Decimal objects
+        obj = {'key': 'value'}
+        expected_output = '{"key": "value"}'
+        output = json.dumps(obj, cls=DecimalEncoder)
+        self.assertEqual(output, expected_output)
+
 if __name__ == '__main__':
     unittest.main()
