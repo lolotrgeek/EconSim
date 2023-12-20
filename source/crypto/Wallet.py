f74766fe4b059adfea5020c04870b183ad216a28
lolotrgeek <jon@simplyenable.com>
2023-12-20 07:03:09 -0600

    feat: update wallet balances

--- source/crypto/Wallet.py
+++ source/crypto/Wallet.py
@@ -14,6 +14,7 @@ class Wallet():
         self.signature_requests = []
         self.chain: dict = {}
         self.assets = {}
+        self.holdings = {}
 
     async def connect(self, chain:str):
         """
@@ -38,9 +39,8 @@ class Wallet():
         return self.assets[asset]['amount']
     
     async def get_assets(self):
-        return await self.crypto_requester.get_assets()
+        return await self.requester.get_assets()
         
-
     async def check_txn(self, txn):
         if txn['sender'] != self.address:
             return {'invalid': txn, 'msg': ('cannot sign transaction, sender address does not match wallet address')}
@@ -62,10 +62,25 @@ class Wallet():
         self.signature_requests.insert(0,txn)
         return {'msg': 'request received'}
 
+    async def update_holdings(self, txn):
+        if txn['asset'] not in self.holdings:
+            self.holdings[txn['asset']] = {'amount': 0}
+        decimals = self.assets[txn['asset']]['decimals']
+
+        if txn['sender'] == self.address:
+            self.holdings[txn['asset']]['amount'] = prec(self.assets[txn['asset']]['amount'] - txn['amount'] - txn['fee'], decimals)
+
+        if txn['recipient'] == self.address:
+            self.holdings[txn['asset']]['amount'] = prec(self.assets[txn['asset']]['amount'] + txn['amount'], decimals)
+
     async def sign_txn(self, txn, decision=False):
         if (await self.check_txn(txn)) == False:
             msg = f'invalid transaction {txn}'
             decision = False
+
+        if decision == True:
+            await self.update_holdings(txn)
+        
         await self.requester.send_signature(decision=decision, txn=txn)
     
     async def set_fee(self):
